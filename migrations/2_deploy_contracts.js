5c76148472796cc9469d9174b451c37c8c13a818
levakin <levakin@protonmail.com>
2018-07-08 09:23:47 +0500

    deploy config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,5 @@
 var VoteFactory = artifacts.require("VoteFactory");
-// var VoteHelper = artifacts.require("VoteHelper");
-var Ownable = artifacts.require("Ownable");
-
 
 module.exports = deployer => {
-    deployer.deploy(Ownable);
-    deployer.link(Ownable, VoteFactory);
     deployer.deploy(VoteFactory);
-    // deployer.link(VoteFactory, VoteHelper);
-    // deployer.deploy(VoteHelper);
 };
