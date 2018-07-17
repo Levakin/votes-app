74a8899e8459872d093680b277a0876e4544387b
levakin <levakin@protonmail.com>
2018-07-17 12:33:06 +0500

    tests

--- test/VoteFactory.js
+++ test/VoteFactory.js
@@ -3,17 +3,20 @@ var expectThrow = require("./helpers/expectThrow");
 var VoteFactory = artifacts.require("VoteFactory");
 
 contract('VoteFactory', async (accounts) => {
+    var instance ;
+
+    beforeEach('setup contract for each test', async function () {
+        instance = await VoteFactory.deployed();
+    });
 
     it("should create new vote correctly", async () => {
-        let instance = await VoteFactory.deployed();
 
         let question = "vote's question";
 
         await instance.createVote(question);
     });
 
-    it("should create two votes and get second question", async() => {
-        let instance = await VoteFactory.deployed();
+    it("should get second question", async() => {
 
         let expectedQuestion = "vote's question";
 
@@ -27,8 +30,7 @@ contract('VoteFactory', async (accounts) => {
         assert.equal(expectedQuestion, createdQuestion, "Vote wasn't created successfully");
     });
 
-    it("should create two votes and change second question", async() => {
-        let instance = await VoteFactory.deployed();
+    it("should change second question", async() => {
         let initialQuestion = "vote's question";
         let expectedQuestion = "changed question";
 
@@ -37,14 +39,13 @@ contract('VoteFactory', async (accounts) => {
         let voteId = (await instance.createVote.call(initialQuestion)).toNumber();
         await instance.createVote(initialQuestion);
 
-        await instance.changeVote(voteId, expectedQuestion);        
+        await instance.setQuestion(voteId, expectedQuestion);        
         let changedQuestion = await instance.getQuestion.call(voteId);
 
         assert.equal(expectedQuestion, changedQuestion, "Vote wasn't changed successfully");
     });
 
-    it("should create vote, add two answers and get correct second answer", async() => {
-        let instance = await VoteFactory.deployed();
+    it("should get correct second answer", async() => {
         
         let question = "vote's question";
         let firstAnswer = "first";
@@ -62,8 +63,7 @@ contract('VoteFactory', async (accounts) => {
         assert.equal(answer, secondAnswer, "Answer wasn't added successfully");
     });
 
-    it("should create vote, add two answers and change second answer", async() => {
-        let instance = await VoteFactory.deployed();
+    it("should change second answer", async() => {
         
         let question = "vote's question";
         let firstAnswer = "first";
@@ -78,14 +78,13 @@ contract('VoteFactory', async (accounts) => {
         let answerId = (await instance.addAnswer.call(voteId, firstAnswer)).toNumber();
         await instance.addAnswer(voteId, secondAnswer);
 
-        await instance.changeAnswer(voteId, answerId, changedAnswer);
+        await instance.setAnswer(voteId, answerId, changedAnswer);
         let answer = await instance.getAnswer(voteId, answerId);
 
         assert.equal(answer, changedAnswer, "Answer wasn't changed successfully");
     });
 
-    it("should create vote, add two answers, two people should vote for second answer", async() => {
-        let instance = await VoteFactory.deployed();
+    it("two people should vote for second answer", async() => {
         
         let question = "vote's question";
         let firstAnswer = "first";
@@ -102,16 +101,14 @@ contract('VoteFactory', async (accounts) => {
 
         await instance.startVote(voteId);
 
-        await instance.vote(voteId, answerId, {from : accounts[1]});
-        await instance.vote(voteId, answerId, {from : accounts[2]});
-        let numOfVoted = (await instance.countVoted.call(voteId, answerId)).toNumber();
+        await instance.cast(voteId, answerId, {from : accounts[1]});
+        await instance.cast(voteId, answerId, {from : accounts[2]});
+        let numCast = (await instance.countCast.call(voteId, answerId)).toNumber();
         
-        assert.equal(numOfVoted, expectedNum, "not 2 voted successfully");
+        assert.equal(numCast, expectedNum, "not 2 voted successfully");
     });
 
-    it("should create vote, add two answers, one person should vote for first answer and revote for second", async() => {
-        let instance = await VoteFactory.deployed();
-        
+    it("person should vote for first answer and revote for second", async() => {
         let question = "vote's question";
         let firstAnswer = "first";
         let secondAnswer = "second";
@@ -128,17 +125,15 @@ contract('VoteFactory', async (accounts) => {
 
         await instance.startVote(voteId);
 
-        await instance.vote(voteId, firstAnswerId, {from : accounts[1]});
-        await instance.vote(voteId, secondAnswerId, {from : accounts[2]});
-        await instance.vote(voteId, secondAnswerId, {from : accounts[1]});
-        let numOfVoted = (await instance.countVoted.call(voteId, secondAnswerId)).toNumber();
+        await instance.cast(voteId, firstAnswerId, {from : accounts[1]});
+        await instance.cast(voteId, secondAnswerId, {from : accounts[2]});
+        await instance.cast(voteId, secondAnswerId, {from : accounts[1]});
+        let numCast = (await instance.countCast.call(voteId, secondAnswerId)).toNumber();
         
-        assert.equal(numOfVoted, expectedNum, "not 2 voted successfully");
+        assert.equal(numCast, expectedNum, "not revoted successfully");
     });
     
     it("should throw when smb doesn't have access to add answers", async() => {
-        let instance = await VoteFactory.deployed();
-
         let question = "vote's question";
         let answer = "answer";
         let voteId = (await instance.createVote.call(question)).toNumber();
@@ -149,7 +144,6 @@ contract('VoteFactory', async (accounts) => {
 
     it("should start and stop vote", async() => {
         let stopped = 2;
-        let instance = await VoteFactory.deployed();
         let question = "vote's question";
 
         let voteId = (await instance.createVote.call(question)).toNumber();
@@ -158,14 +152,39 @@ contract('VoteFactory', async (accounts) => {
         await instance.stopVote(voteId);
 
         let state = (await instance.getState.call(voteId)).toNumber();
-        console.log(state);
         assert.equal(state, stopped, "not stopped");
+    });
+
+    it("should get correct results", async() => {
+        let question = "vote's question";
+        let firstAnswer = "first";
+        let secondAnswer = "second";
+
+        await instance.createVote(question);
+        let voteId = (await instance.createVote.call(question)).toNumber();
+        await instance.createVote(question);
+
+        await instance.addAnswer(voteId, firstAnswer);
+
+        let answerId = (await instance.addAnswer.call(voteId, secondAnswer)).toNumber();
+        await instance.addAnswer(voteId, secondAnswer);
+
+        await instance.startVote(voteId);
+
+        await instance.cast(voteId, answerId, {from : accounts[1]});
+        await instance.cast(voteId, answerId, {from : accounts[2]});
+        
+        await instance.stopVote(voteId);
+
+
+        let result = await instance.getResultsQuestion.call(voteId);
+        
+        assert.equal(result, secondAnswer, "not correct result");
     });
 });
 
 /*
     it("", async() => {
-        let instance = await VoteFactory.deployed();
 
     });
 */
